866d075b981e87d7dc25efdcc4f7aa684a84f210
antonella1986 <antonella.leo.mail@gmail.com>
2025-07-28 11:42:03 +0200

    add favourites style

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -7,18 +7,18 @@ export default function Favourites() {
 
     return (
         <>           
-            <h1 className="text-center mb-4">I tuoi prodotti preferiti</h1>
+            <h1 className="text-center mb-5 mt-5"><strong>I tuoi prodotti preferiti</strong></h1>
             <div className="container">
                 <div className="row">
                     {favouriteProducts.map((product) => (
-                        <div key={product.id} className="col-md-4 mb-3">
+                        <div key={product.id} className="col-md-4 mb-5">
                             <div className="card">
                                 <div className="card-body">
                                 <img 
                                     src={product.imageUrl} 
                                     alt={product.title}
-                                    className="img-fluid rounded"
-                                    style={{ maxHeight: '400px', objectFit: 'cover' }}
+                                    className="img-fluid rounded mx-auto d-block mb-3"
+                                    style={{ maxHeight: '250px', objectFit: 'cover', objectPosition: 'center' }}
                                 />
                                     <h5 className="card-title">
                                         <strong>{product.title}</strong>
@@ -26,12 +26,12 @@ export default function Favourites() {
                                     <p className="card-text">Categoria: {product.category}</p>
                                         <Link 
                                             to={`/products/${product.id}`} 
-                                            className="btn btn-primary"
+                                            className="btn btn-products"
                                         >
                                             Dettagli
                                         </Link>
-                                    <button onClick={() => removeFromFavourites(product.id)} className="btn btn-lg">
-                                        🗑️
+                                    <button onClick={() => removeFromFavourites(product.id)} className="btn btn-remove mx-3">
+                                        Rimuovi
                                     </button>
                                 </div>
                             </div>
